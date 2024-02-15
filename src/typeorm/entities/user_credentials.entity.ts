7f80e11835129fe5f05fe1cd38a61e195f12e497
Shashwat Sarkar <shashwat@ncompass.inc>
2024-02-15 10:34:03 +0530

    user related entities created

---
+++ src/typeorm/entities/user_credentials.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('USER_CREDENTIALS')
+export class UserCredential {
+  @PrimaryGeneratedColumn({ name: 'ID' })
+  id: number;
+
+  @Column({ name: 'PASSWORD' })
+  password: string;
+
+  @CreateDateColumn({ name: 'CREATED_AT' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'UPDATED_AT' })
+  updatedAt: Date;
+}
---
+++ src/typeorm/entities/users.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('USERS')
+export class User {
+  @PrimaryColumn({ name: 'ID', length: 200 })
+  id: string;
+
+  @Column({ name: 'NAME', length: 200 })
+  name: string;
+
+  @Column({ name: 'EMAIL', length: 200 })
+  email: string;
+
+  @Column({ name: 'ROLE_ID', length: 200 })
+  roleId: string;
+
+  @CreateDateColumn({ name: 'CREATED_AT' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'UPDATED_AT' })
+  updatedAt: Date;
+}
