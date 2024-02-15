cb7f7c5aaefd9143e957b4c1eec69dae12eef686
Shashwat Sarkar <shashwat@ncompass.inc>
2024-02-15 15:40:45 +0530

    login created

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,15 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+
 import { User } from 'src/typeorm/entities/users.entity';
+import { UserCredential } from 'src/typeorm/entities/user_credentials.entity';
+import { jwtConstants } from './constants';
+
 
 @Module({
-  imports:[
+  imports: [
     TypeOrmModule.forFeature([
+      UserCredential,
       User
-    ])
+    ]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60m' }
+    }),
   ],
   providers: [AuthService],
   controllers: [AuthController]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpStatus, HttpException, Injectable } from '@nestjs/common';
 import { createUserParams } from './types/createUserParams';
 import { createUserPasswordParams } from './types/createUserPasswordParams';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/users.entity';
 import { Repository } from 'typeorm';
 import { Md5 } from 'ts-md5';
+import { JwtService } from '@nestjs/jwt';
 import { UserCredential, UserCredentialData } from 'src/typeorm/entities/user_credentials.entity';
 import { customError } from 'src/utils/exceptionHandler';
 import { UserRole } from 'src/typeorm/entities/user_roles.entity';
@@ -12,8 +13,10 @@ import { createRoleDto } from './dtos/createRole.dto';
 
 @Injectable()
 export class AuthService {
-
+    constructor(private jwtService: JwtService) { };
     @InjectRepository(User) private userRepository: Repository<User>
+    @InjectRepository(UserCredential) private userCredentialRepository: Repository<UserCredential>
+
 
     async createUser(userDetails: createUserParams, userPasswordDetails: createUserPasswordParams) {
         try {
@@ -53,5 +56,28 @@ export class AuthService {
         } catch (error) {
             return new customError(HttpStatus.INTERNAL_SERVER_ERROR, "Some Error Occured", error.message)
         }
+    }
+
+    async validateUser(email: string, password: string): Promise<any> {
+        const user = this.userRepository.findOne({ where: { email } });
+        if (!user)
+            throw new HttpException('Invalid Credentials', HttpStatus.UNAUTHORIZED);
+
+        const passwordHash = Md5.hashStr(password);
+        const userCredential = this.userCredentialRepository.findOne({ where: { id: (await user).id } });
+
+        if (!userCredential || passwordHash != (await userCredential).password)
+            throw new HttpException('Invalid Credentials', HttpStatus.FORBIDDEN);
+
+        return user
+
+    }
+
+    async login(email: string, password: string): Promise<any> {
+        const user = await this.validateUser(email, password);
+        console.log(user)
+        const payload = { email: (user).email, id: (user).id };
+        const token = await this.jwtService.signAsync(payload);
+        return token;
     }
 }
