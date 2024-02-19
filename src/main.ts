03d6050707ff37da2c38a5947b05b1bf73b8e370
rajatkumar-ncompass <rajatkumar@ncompass.inc>
2024-02-19 15:25:57 +0530

    Port Change

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new MyExceptionsFilter());
 
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();
